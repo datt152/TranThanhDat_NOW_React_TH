817c3499e94fc381ce692d11639c392ae11a9a71
H82M19\Student <Student>
2025-04-16 11:31:23 +0700

    finish

--- project_week_09/src/main.jsx
+++ project_week_09/src/main.jsx
@@ -6,16 +6,13 @@ import CounterApp from './components/counter_app.jsx'
 import TodoApp from './components/todo_app.jsx'
 import ThemeApp from './components/theme_app.jsx'
 import ShoppingCart from './components/shopping_cart.jsx'
-import AdminApp from './components/admin_app.jsx'
-
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <App /> */}
-    {/* <CounterApp/> */}
-    {/* <TodoApp/> */}
-    {/* <ThemeApp/> */}
-    {/* <ShoppingCart/> */}
-    <AdminApp/>
+    <CounterApp/>
+    <TodoApp/>
+    <ThemeApp/>
+    <ShoppingCart/>
   </StrictMode>,
 )
